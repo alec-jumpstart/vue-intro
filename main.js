c4ec4425a56bae2032d47148c716e3064fd3e32e
Alec Johnson <alec@jumpstart.me>
2021-05-22 14:58:15 -0700

    don't render no reviews if there are reviews

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ Vue.component('product', {
       </div>
       <div>
         <h2>Reviews</h2>
-        <p>There are no reviews yet.</p>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
         <ul>
           <li v-for="review in reviews">
             <p>{{ review.name }}</p>
