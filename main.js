85e306118bbc4ed0d5e4673733e466f995660fc8
Alec Johnson <alec@jumpstart.me>
2021-05-22 13:57:05 -0700

    move product into a component

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    brand: 'Vue Masterful',
-    product: 'Socks',
-    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-    selectedVariant: 0,
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: "green",
-        variantImage: './assets/vmSocks-green.jpg',
-        variantQuantity: 0,
-      },
-      {
-        variantId: 2235,
-        variantColor: "blue",
-        variantImage: './assets/vmSocks-blue.jpg',
-        variantQuantity: 10,
-      }
-    ],
-    cart: 0,
+Vue.component('product', {
+  template: `
+  <div class="product">
+      <div class="product-image">
+          <img :src="image">
+      </div>
+      <div class="product-info">
+          <h1>{{ title }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else :class="{ lineThrough: !inStock }">Out of Stock</p>
+
+          <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+          </ul>
+
+          <div v-for="(variant, index) in variants" :key="variant.variantId" class="color-box"
+          :style="{ backgroundColor: variant.variantColor }" @mouseover="updateProduct(index)">
+          </div>
+
+          <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">
+          Add to Cart</button>
+          <div class="cart">
+          <p>Cart({{ cart }})</p>
+          </div>
+      </div>
+  </div>
+  `,
+  data() {
+    return {
+      brand: 'Vue Masterful',
+      product: 'Socks',
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      selectedVariant: 0,
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImage: './assets/vmSocks-green.jpg',
+          variantQuantity: 0,
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImage: './assets/vmSocks-blue.jpg',
+          variantQuantity: 10,
+        }
+      ],
+      cart: 0,
+    }
   },
   methods: {
     addToCart() {
@@ -40,4 +67,9 @@ var app = new Vue({
       return this.variants[this.selectedVariant].variantQuantity > 0
     }
   }
+})
+
+
+var app = new Vue({
+  el: '#app',
 })
